d375005ab94d7db7e175fc84fcd122ca5a0f27de
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2021-03-06 11:33:29 -0500

    Added snake drawing

--- snake.py
+++ snake.py
@@ -7,6 +7,7 @@ Created on
 import pygame
 from pygame.locals import *
 import random
+import numpy
 
 
 # Colors
@@ -18,7 +19,40 @@ BLACK = (0, 0, 0)
 
 pygame.init()
 
-SURFACE = pygame.display.set_mode((800,800))
+
+FIELDWIDTH = 10
+FIELDHEIGHT = 10
+
+field = numpy.zeros((FIELDHEIGHT, FIELDWIDTH), dtype=numpy.int16)
+field[3][4] = 3
+field[4][4] = 2
+field[5][4] = 2
+field[6][4] = 2
+field[7][4] = 1
+
+
+SURFACE = pygame.display.set_mode((32 * FIELDWIDTH, 32 * FIELDHEIGHT))
+
+
+
+snake = pygame.image.load("snake.png")
+
+head = snake.subsurface(3 * 64, 0, 64, 64)
+head = pygame.transform.scale(head, (32, 32))
+body = snake.subsurface(2 * 64, 64, 64, 64)
+body = pygame.transform.scale(body, (32, 32))
+tail = snake.subsurface(3 * 64, 2 * 64, 64, 64)
+tail = pygame.transform.scale(tail, (32, 32))
+def drawField():
+    for i in range(field.shape[0]):
+        for j in range(field.shape[1]):
+            if field[i][j] == 1:
+                SURFACE.blit(tail, (j * 32, i * 32))
+            elif field[i][j] == 2:
+                SURFACE.blit(body, (j * 32, i * 32))
+            elif field[i][j] == 3:
+                SURFACE.blit(head, (j * 32, i * 32))
+                
 
 isRunning = True
 while isRunning:
@@ -30,7 +64,7 @@ while isRunning:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 isRunning = False
-
+    drawField()
     pygame.display.update()
 
 pygame.quit()
